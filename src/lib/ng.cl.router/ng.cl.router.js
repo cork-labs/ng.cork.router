a9de09eabcd89ea671fcba4d620a247234e787ed
Andre Torgal <andre.torgal@gmail.com>
2015-02-07 23:13:00 +0000

    chore(docs): tweak css, color code links; wording(s)

--- src/lib/ng.cl.router/ng.cl.router.js
+++ src/lib/ng.cl.router/ng.cl.router.js
@@ -13,7 +13,7 @@
      * - ngRoute
      *
      * @description
-     * Allows the {@link clRouter} service to be configured and routes to be registered in `$routeProvider` during the
+     * Allows the {@link ng.cl.router.clRouter clRouter} service to be configured and routes to be registered in `$routeProvider` during the config phase of the application.
      * config phase of your application.
      *
      * @property {Object} $routeProvider Reference to AngularJS `$routeProvider` provider. Avoids having to inject both `clRouterProvider` and `$routeProvider` in case you need some underlying feature like `$route.current`.
@@ -120,7 +120,7 @@
              * @methodOf ng.cl.router.clRouterProvider
              *
              * @description
-             * Registers the route in both angular `$routeProvider` and the {@link clRouterProvider} provider.
+             * Registers the route in both angular `$routeProvider` and the {@link ng.cl.router.clRouterProvider clRouterProvider} provider.
              *
              * @param {string} route  The route name.
              * @param {object} config The $routeProvider object, extended with a `path` attribute that corresponds to the `path` argument to {@link https://docs.angularjs.org/api/ngRoute/provider/$routeProvider#when $routeProvider:when()}.
